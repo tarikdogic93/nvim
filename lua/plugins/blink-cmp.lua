c1f94fa46c430235eacbe5fa92164c1a7b0fcdec
tarikdogic93 <tarikdogic93@gmail.com>
2025-05-11 19:05:14 +0200

    Changed the key binding for accepting suggestion.

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -41,7 +41,7 @@ return {
 			-- See :h blink-cmp-config-keymap for defining your own keymap
 			keymap = {
 				preset = "default",
-				["<C-Z>"] = { "accept", "fallback" },
+				["<Enter>"] = { "accept", "fallback" },
 			},
 
 			appearance = {
